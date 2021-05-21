f533ec7eb56bef397025c9c68e5931f391b54636
Henry Gabriel <henrygbrieloliveira@gmail.com>
2021-05-21 14:44:33 -0300

    Diminuindo intervalo de troca de

--- principal.js
+++ principal.js
@@ -1,3 +1,5 @@
+//inicio do arquivo...
+setInterval(trocaBanner, 1000);
 var banners =
 ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
@@ -6,5 +8,3 @@ bannerAtual = (bannerAtual + 1) % 2;
 document.querySelector('h2#mensagem').textContent =
 banners[bannerAtual];
 }
-
-setInterval(trocaBanner, 2000);
